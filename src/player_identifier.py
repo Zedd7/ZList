48b18df806d6e97eee4f5566f4d1d983a654fb03
Zedd7 <zedd@mail.be>
2018-04-05 17:33:45 +0200

    Refactored player_identifier

--- src/player_identifier.py
+++ src/player_identifier.py
@@ -9,92 +9,95 @@ import csv
 import requests
 
 APP_ID = 'demo'  # Replace with your own app id to extend the requests limit
-REQUEST_URL = 'https://api.worldoftanks.eu/wot/account/list/'
+ACCOUNT_INFO_REQUEST_URL = 'https://api.worldoftanks.eu/wot/account/list/'
 BATCH_SIZE = 100
-ZLIST_FOLDER = "../res_mods/mods/shared_resources/xvm/res/clanicons/EU/nick"
+ZLIST_FOLDER = '../res_mods/mods/shared_resources/xvm/res/clanicons/EU/nick'
 OUTPUT_FOLDER = '../data'
-OUTPUT_FILE = '%s/ZLIST_ID.csv' % OUTPUT_FOLDER
+OUTPUT_FILE = '{folder}/ZLIST_ID.csv'.format(folder=OUTPUT_FOLDER)
 UNKNOWN_ID = -1
 
 
-def get_player_list(player_folder):
+def get_player_names(player_folder):
     """Extract player names from ZList."""
     print("Extracting player names from ZList... ", end='')
-    player_list = []
+    player_names = []
     for file_name in os.listdir(player_folder):
         if not file_name.startswith('.'):  # Is not a category file
-            player_list.append(file_name.rstrip('.png'))
+            player_names.append(file_name.rstrip('.png'))
     print("Done.")
-    return player_list
+    return player_names
 
 
-def register_player_ids(player_list, output_file_path):
-    """Map player names to their id in a CSV file."""
+def get_player_ids(player_names, output_file_path):
+    """Get account id of players."""
     # Retrieve previously registered player ids
-    player_id_dict = load_player_id_dict(output_file_path)
-
-    player_count = len(player_list)
-    index = 0
-    while index < len(player_list):
+    player_ids = load_player_ids(output_file_path)
+    index, player_count = 0, len(player_names)
+    while index < len(player_names):
         batch = []
-        while len(batch) < BATCH_SIZE and index < len(player_list):
-            player = player_list[index]
-            if player not in player_id_dict:
-                batch.append(player)
+        while len(batch) < BATCH_SIZE and index < len(player_names):
+            player_name = player_names[index]
+            if player_name not in player_ids:
+                batch.append(player_name)
             index += 1
         if batch:
-            batch_id_dict = get_player_id_dict(batch)
-            for player in batch_id_dict:
-                player_id_dict[player] = batch_id_dict[player]
+            batch_ids = fetch_player_ids(batch)
+            for player_name in batch_ids:
+                player_ids[player_name] = batch_ids[player_name]
         progress = index / player_count * 100
         sys.stdout.write("\rRequesting player ids : %.2f %%" % progress)
         sys.stdout.flush()
     print()
-
-    print("Registering player ids to CSV file... ", end='')
-    with open(output_file_path, 'w', newline='') as output_file:
-        csv_writer = csv.writer(output_file, delimiter=',')
-        for player in sorted(player_id_dict.keys(), key=str.lower):
-            csv_writer.writerow([player, player_id_dict[player]])
-    print("Done.")
+    return player_ids
 
 
-def load_player_id_dict(output_file_path):
+def load_player_ids(output_file_path):
     """Load dictionary of known mappings for player names and their id."""
     print("Loading registered player ids from CSV file... ", end='')
-    player_id_dict = {}
+    player_ids = {}
     with open(output_file_path, 'r', newline='') as output_file:
         csv_reader = csv.reader(output_file, delimiter=',')
-        for player, player_id in csv_reader:
-            player_id_dict[player] = player_id
+        for player_name, player_id in csv_reader:
+            player_ids[player_name] = player_id
     print("Done.")
-    return player_id_dict
+    return player_ids
 
 
-def get_player_id_dict(player_list):
-    """Retrieve the ids of a list of players."""
+def fetch_player_ids(player_names):
+    """Retrieve the ids of a batch of players."""
     payload = {
         'application_id': APP_ID,
-        'search': ','.join(player_list),
+        'search': ','.join(player_names),
         'type': 'exact'
     }
-    response = requests.get(REQUEST_URL, params=payload)
+    response = requests.get(ACCOUNT_INFO_REQUEST_URL, params=payload)
     response_content = response.json()
-    player_id_dict = {player: UNKNOWN_ID for player in player_list}
+
+    player_ids = {player: UNKNOWN_ID for player in player_names}
     if response_content['status'] == 'ok':
         for player_data in response_content['data']:
-            player = get_player_name(player_data['nickname'], player_list)
+            player = get_player_name(player_data['nickname'], player_names)
             player_id = player_data['account_id']
-            player_id_dict[player] = player_id
-    return player_id_dict
+            player_ids[player] = player_id
+    return player_ids
 
 
-def get_player_name(player_name, player_list):
+def get_player_name(player_name, player_names):
     """Return the name of the player in the same case as in the list."""
-    for player in player_list:
+    for player in player_names:
         if player.lower() == player_name.lower():
             return player
     return player_name
+
+
+def register_player_ids(player_ids, output_file_path):
+    """Register player ids in CSV file."""
+    print("Registering player ids to CSV file... ", end='')
+    with open(output_file_path, 'w', newline='') as output_file:
+        csv_writer = csv.writer(output_file, delimiter=',')
+        for player in sorted(player_ids.keys(), key=str.lower):
+            csv_writer.writerow([player, player_ids[player]])
+    print("Done.")
 
 
 if __name__ == "__main__":
@@ -106,5 +109,6 @@ if __name__ == "__main__":
     if not os.path.exists(OUTPUT_FILE):
         open(OUTPUT_FILE, 'w').close()
 
-    player_list = get_player_list(ZLIST_FOLDER)
-    register_player_ids(player_list, OUTPUT_FILE)
+    player_names = get_player_names(ZLIST_FOLDER)
+    player_ids = get_player_ids(player_names, OUTPUT_FILE)
+    register_player_ids(player_ids, OUTPUT_FILE)
