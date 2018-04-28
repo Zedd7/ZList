9a86c326ef6177ce7969c7f868ed37102317252a
Zedd7 <zedd@mail.be>
2018-04-28 20:32:31 +0200

    Added mod for the logging of gold users in battle

--- src/mod/gold_logger.py
+++ src/mod/gold_logger.py
@@ -1,42 +1,43 @@
+"""WoT mod for logging gold users into the ZList. Forked from https://wgmods.net/1233/."""
+
 import re
-import json
 import BigWorld
 import nations
 import ResMgr
 from functools import wraps, partial
-from items import vehicles, _xml
+from items import _xml
 from constants import ITEM_DEFS_PATH
-from helpers.EffectsList import EffectsList, _PixieEffectDesc
+from helpers.EffectsList import _PixieEffectDesc
 from helpers.bound_effects import ModelBoundEffects, StaticSceneBoundEffects
 
-
-# load and parse json file
-def load_json_from_file(file_path):
-    try:
-        with open(file_path, 'r') as json_file:
-            return json.loads(json_file.read())
-    except (IOError, ValueError):
-        return {}
+import os
 
 
-config = load_json_from_file('res_mods/configs/goldvisibility2.json')
+ZLIST_FOLDER = './res_mods/mods/shared_resources/xvm/res/clanicons/EU/nick'
+LOG_FILE = './GOLD_USER.csv'
 
 
-# Keep track of mutations
+def prepare_log_file():
+    if not os.path.exists(LOG_FILE):
+        open(LOG_FILE, 'w').close()
+
+
+def log_gold_user(player_name, account_id):
+    with open(LOG_FILE, 'a') as log_file:
+        log_file.write("%s,%s\n" % (player_name, account_id))
+
+
 class ModifiedValueManager:
     def __init__(self):
         self._values = []
-
     def add_value(self, container, key, restore_value):
         self._values.append((container, key, restore_value))
-
     def restore(self):
         for (container, key, restore_value) in self._values:
             container[key] = restore_value
         self._values = []
 
 
-# decorator for running callback before the specified function is called
 def run_before(module, func_name):
     def decorator(callback):
         func = getattr(module, func_name)
@@ -52,26 +53,20 @@ def run_before(module, func_name):
     return decorator
 
 
-# Load shell prices for a nation
-# Side effect ResMgr
 def load_shell_prices(nation):
     xml_path = ITEM_DEFS_PATH + 'vehicles/' + nation + '/components/shells.xml'
     section = ResMgr.openSection(xml_path)
-
     prices = {}
     for name, subsection in section.items():
         if name in ('icons', 'xmlns:xmlref'):
             continue
-
         xml_ctx = (None, xml_path + '/' + name)
         shell_id = _xml.readInt(xml_ctx, subsection, 'id', 0, 65535)
         prices[shell_id] = _xml.readPrice(xml_ctx, subsection, 'price')
-
     ResMgr.purge(xml_path, True)
     return prices
 
 
-# get particle effects for effects list
 def get_pixie_effects(effects_list):
     effects_desc = effects_list._EffectsList__effectDescList
     return [desc for desc in effects_desc if isinstance(desc, _PixieEffectDesc)]
@@ -79,107 +74,67 @@ def get_pixie_effects(effects_list):
 
 def get_gun(attacker_id, players):
     attacker = players.get(attacker_id, None)
-
     if attacker is None:
         return None
-
     gun, _ = attacker['vehicleType'].getComponentsByType('vehicleGun')
-
     return gun
 
 
-# return gold ammo shell types for player
 def get_gold_ammo_types_from_prices(shell_prices, default_gold_ammo_types, gun):
     if gun is None:
         return default_gold_ammo_types
-
     gold_ammo_types = []
     for shot in gun.shots:
         nation_id, shell_id = shot.shell.id
         gold_price = shell_prices[nation_id][shell_id].get('gold', None)
         if gold_price is not None:
             gold_ammo_types.append(shot.shell.kind)
-
     if len(gold_ammo_types) > 0:
         return gold_ammo_types
-
-    # no shell buyable with gold
-    # shell is considered 'gold' if credits price is higher then the avg shell price for the gun
-    # WG removed gold price in patch 1.0.1 for gold shells
     credits_prices = []
     for shot in gun.shots:
         nation_id, shell_id = shot.shell.id
         credits_price = shell_prices[nation_id][shell_id].get('credits', 0)
         credits_prices.append(credits_price)
-
     avg_price = reduce(lambda total, price: total + price, credits_prices, 0) / len(credits_prices)
     premium_shells = [index for (index, price) in enumerate(credits_prices) if price > avg_price]
-
     return map(lambda index: gun.shots[index].shell.kind, premium_shells)
 
 
 get_gold_ammo_types = partial(
     get_gold_ammo_types_from_prices,
     map(lambda nation: load_shell_prices(nation), nations.NAMES),
-    config.get('gold_ammo_types_for_unspotted_vehicles', [])
+    []
 )
 
 
-# return True if the gun uses the gold shell type as standard shell type as well
-def is_gold_shell_type_ambiguous(shell_type, gun):
-    if gun is None:
-        return False
-
-    gold_shell_types = get_gold_ammo_types(gun)
-
-    if shell_type not in gold_shell_types:
-        return False
-
-    shell_types = map(lambda s: s.shell.kind, gun.shots)
-
-    occ_in_shell_types = reduce(lambda total, st: int(st == shell_type) + total, shell_types, 0)
-    occ_in_gold_shell_types = reduce(lambda total, st: int(st == shell_type) + total, gold_shell_types, 0)
-
-    return occ_in_shell_types > occ_in_gold_shell_types
-
-
-# in game id is different form the player().id
 def get_ingame_player_id(player, players):
     player_name = player.name
     return [player_id for (player_id, player) in players.items() if player['name'] == player_name][0]
-
-
-def is_own_player(attacker_id, player, players):
-    return get_ingame_player_id(player, players) == attacker_id
 
 
 def get_team_id(player_id, players):
     return players.get(player_id, {}).get('team', None)
 
 
-def is_allied_player(attacker_id, player, players):
-    own_team_id = get_team_id(get_ingame_player_id(player, players), players)
-    attacker_team_id = get_team_id(attacker_id, players)
-    return attacker_team_id == own_team_id and own_team_id is not None
+def should_log_attacker(player, players, shell_type, gun, attacker_id):
+    return not(is_own_player(attacker_id, player, players) or is_gold_shell_type_ambiguous(shell_type, gun))
 
 
-def is_team_member(attacker_id, player, players):
-    return is_allied_player(attacker_id, player, players) and not is_own_player(attacker_id, player, players)
+def is_own_player(attacker_id, player, players):
+    return get_ingame_player_id(player, players) == attacker_id
 
 
-def settings_effect_must_show(display_settings, player, players, shell_type, gun, attacker_id):
-    return (not is_gold_shell_type_ambiguous(shell_type, gun) or display_settings['display_if_ambiguous']) \
-           and (not is_own_player(attacker_id, player, players) or display_settings['display_for_own']) \
-           and (not is_team_member(attacker_id, player, players) or display_settings['display_for_allies']) \
-           and (is_allied_player(attacker_id, player, players) or display_settings['display_for_enemy'])
-
-
-effect_must_show = partial(settings_effect_must_show, {
-    'display_if_ambiguous': config.get('display_effect_if_ammo_type_ambiguous', False),
-    'display_for_own': config.get('display_effect_for_own_shots', False),
-    'display_for_allies': config.get('display_effect_for_allied_shots', False),
-    'display_for_enemy': config.get('display_effect_for_enemy_shots', True)
-})
+def is_gold_shell_type_ambiguous(shell_type, gun):
+    if gun is None:
+        return False
+    gold_shell_types = get_gold_ammo_types(gun)
+    if shell_type not in gold_shell_types:
+        return False
+    shell_types = map(lambda s: s.shell.kind, gun.shots)
+    occ_in_shell_types = reduce(lambda total, st: int(st == shell_type) + total, shell_types, 0)
+    occ_in_gold_shell_types = reduce(lambda total, st: int(st == shell_type) + total, gold_shell_types, 0)
+    return occ_in_shell_types > occ_in_gold_shell_types
 
 
 def shell_type_from_effect_name(effect_name):
@@ -193,30 +148,26 @@ def shell_type_from_effect_name(effect_name):
     return shell_type
 
 
-# Restore effect and modify PixieEffectDescription file names to point to modified effects for gold shells
 def restore_with_mgr_and_modify_effect(modified_file_name_mgr, effects_list, attacker_id):
-    # restore modified effects to their default
     modified_file_name_mgr.restore()
-
     player = BigWorld.player()
     players = player.arena.vehicles
-
     gun = get_gun(attacker_id, players)
     gold_ammo_types = get_gold_ammo_types(gun)
-
     pixie_effects = get_pixie_effects(effects_list)
-
     for pixie_effect in pixie_effects:
         for (index, file_path) in enumerate(pixie_effect._files):
             match = re.search('^particles/Shells_Eff/([a-zA-Z0-9_-]+)\.xml$', file_path)
             if match is not None:
                 effect_name = match.group(1)
                 shell_type = shell_type_from_effect_name(effect_name)
+                if shell_type in gold_ammo_types and should_log_attacker(player, players, shell_type, gun, attacker_id):
+                    attacker_name = players[attacker_id]['name']
+                    attacker_dbid = players[attacker_id]['accountDBID']
+                    log_gold_user(attacker_name, attacker_dbid)
 
-                if shell_type in gold_ammo_types and effect_must_show(player, players, shell_type, gun, attacker_id):
-                    modified_file_name_mgr.add_value(pixie_effect._files, index, pixie_effect._files[index])
-                    pixie_effect._files[index] = 'particles/Shells_Eff/' + effect_name + '_prem.xml'
 
+prepare_log_file()
 
 restore_and_modify_effect = partial(restore_with_mgr_and_modify_effect, ModifiedValueManager())
 
